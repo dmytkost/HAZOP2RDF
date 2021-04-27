7cb3b82a9eb51768ed3bc94194401c42b4ebc9bc
Dmytro Kostiuk <dimak@My-MBP.local>
2021-04-27 13:59:39 +0200

    HAZOP update to match the ontology graph

--- src/importer.py
+++ src/importer.py
@@ -46,35 +46,35 @@ class GraphMaker:
             uri = "http://www.cae-pa.de/HAZOPCase/" + hazop_case
 
             reference   = URIRef(uri)
-            deviation   = URIRef(reference + "/Deviation")
-            cause       = URIRef(reference + "/Cause")
-            consequence = URIRef(reference + "/Consequence")
-            safeguard   = URIRef(reference + "/Safeguard")
+            deviation   = BNode()
+            cause       = BNode()
+            consequence = BNode()
+            safeguard   = BNode()
 
             g.add((reference, n.Deviation, deviation))
             g.add((reference, n.Cause, cause))
             g.add((reference, n.Consequence, consequence))
-            g.add((reference, n.safeguard, safeguard))
+            g.add((reference, n.Safeguard, safeguard))
 
             g.add((deviation, n.HAZOPNode, Literal(row[1])))
             g.add((deviation, n.Parameter, Literal(row[2])))
-            g.add((deviation, n.Consequence, Literal(row[3])))
-            g.add((deviation, n.Safeguard, Literal(row[4])))
+            g.add((deviation, n.Guideword, Literal(row[3])))
+            g.add((deviation, n.Description, Literal(row[4])))
 
             g.add((cause, n.HAZOPNode, Literal(row[5])))
             g.add((cause, n.Parameter, Literal(row[6])))
-            g.add((cause, n.Consequence, Literal(row[7])))
-            g.add((cause, n.Safeguard, Literal(row[8])))
+            g.add((cause, n.Guideword, Literal(row[7])))
+            g.add((cause, n.Description, Literal(row[8])))
 
             g.add((consequence, n.HAZOPNode, Literal(row[9])))
             g.add((consequence, n.Parameter, Literal(row[10])))
-            g.add((consequence, n.Consequence, Literal(row[11])))
-            g.add((consequence, n.Safeguard, Literal(row[12])))
+            g.add((consequence, n.Guideword, Literal(row[11])))
+            g.add((consequence, n.Description, Literal(row[12])))
 
             g.add((safeguard, n.HAZOPNode, Literal(row[13])))
             g.add((safeguard, n.Parameter, Literal(row[14])))
-            g.add((safeguard, n.Consequence, Literal(row[15])))
-            g.add((safeguard, n.Safeguard, Literal(row[16])))
+            g.add((safeguard, n.Guideword, Literal(row[15])))
+            g.add((safeguard, n.Description, Literal(row[16])))
 
         with open("data/mHAZOP_Dosier_PEA.ttl", "w") as file:
             file.write(g.serialize(format="turtle").decode("utf-8"))
