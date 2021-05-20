a28e8d972957982cae17f20d2162f34f04d5a5df
Dmytro Kostiuk <dimak@My-MBP.local>
2021-05-20 20:43:18 +0200

    Added TripleStore API
    Commands: upload, get, get graph bindings.

---
+++ src/services/svc_triplestore.py
@@ -0,0 +1,29 @@
+import os, json
+
+
+class Service:
+    url = "http://localhost:3030/ds"
+
+    def upload_turtle_file(self, filename, filepath):
+        cmd = f"s-put {self.url} {filename} {filepath}"
+        response = os.system(cmd)
+
+        return response
+
+    def get_turtle_file(self, filename):
+        cmd = f"s-get {self.url} {filename}"
+        response = os.popen(cmd).read()
+
+        return response
+
+    def get_all_graph_bindings(self):
+        rq = "src/queries/bindings.rq"
+        cmd = f"s-query --service={self.url}/query --query={rq}"
+        response = os.popen(cmd).read()
+        response_dict = json.loads(response)
+
+        bindings = []
+        for g in response_dict["results"]["bindings"]:
+            bindings.append(g["g"]["value"])
+
+        return bindings
