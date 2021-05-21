60e27c321a8fcdd2081135e9cf20cbb6f44a805b
Dmytro Kostiuk <dimak@My-MBP.local>
2021-05-21 02:34:20 +0200

    Added Exporter CLI
    Added interface with commands:
    
    cli exporter cmd_export_rdf_graphs_from_local_directory
    Exports data in excel format from local directory
    
    cli exporter cmd_export_rdf_graphs_from_fuseki_server
    Exports data in excel format from fuseki server, if its running
    
    Updated SPARQL query to feel the dataframe with all parameters
    
    Added new header für Dataframe (no Multiindex columns)

---
+++ src/services/svc_exporter.py
@@ -0,0 +1,24 @@
+import glob, pandas as pd
+from rdflib import Graph
+
+
+class Service:
+    def read_local_directory(self):
+        return glob.glob("data/turtle/*.ttl")
+
+    def create_hazop_dataframe(self, graph, index):
+        df = pd.DataFrame(columns=index)
+
+        g = Graph()
+        g.parse(data=graph, format="turtle")
+
+        with open("src/queries/hazop.rq", "r") as f:
+            qres = g.query(f.read())
+
+        for i, row in enumerate(qres):
+            df.loc[i] = row
+
+        return df
+
+    def export_to_excel(self, df, filename):
+        df.to_excel(f"data/excel/{filename}", index=False)
--- src/services/svc_triplestore.py
+++ src/services/svc_triplestore.py
@@ -4,19 +4,19 @@ import os, json
 class Service:
     url = "http://localhost:3030/ds"
 
-    def upload_turtle_file(self, filename, filepath):
+    def upload_hazop_graph(self, filename, filepath):
         cmd = f"s-put {self.url} {filename} {filepath}"
         response = os.system(cmd)
 
         return response
 
-    def get_turtle_file(self, filename):
+    def get_hazop_graph(self, filename):
         cmd = f"s-get {self.url} {filename}"
         response = os.popen(cmd).read()
 
         return response
 
-    def get_all_graph_bindings(self):
+    def get_hazop_graph_bindings(self):
         rq = "src/queries/bindings.rq"
         cmd = f"s-query --service={self.url}/query --query={rq}"
         response = os.popen(cmd).read()
