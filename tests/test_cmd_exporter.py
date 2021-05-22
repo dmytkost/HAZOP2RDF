078728768de668c92af85df837d53c09394ccfa3
Dmytro Kostiuk <dimak@My-MBP.local>
2021-05-22 17:21:53 +0200

    Added Exporter CLI tests

---
+++ tests/test_cmd_exporter.py
@@ -0,0 +1,17 @@
+from click.testing import CliRunner
+from src.commands.cmd_exporter import cli
+
+
+def test_export_graphs_from_local_directory():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["cmd-export-graphs-from-local-directory"])
+    assert result.exit_code == 0
+    assert not result.exception
+    assert "PEA-HAZOP-Dosiermodul_v07.xlsx" in result.output
+
+
+def test_export_graphs_from_fuseki_server():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["cmd-export-graphs-from-fuseki-server"])
+    assert result.exit_code == 0 or result.exit_code == 1
+    assert "PEA-HAZOP-Dosiermodul_v07.xlsx" or "Error: Failed connection to Fuseki server" in result.output
