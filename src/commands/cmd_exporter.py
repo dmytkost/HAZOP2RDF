17c6d5d28b4dda17a11380ee302ec27eb56af57f
Dmytro Kostiuk <dimak@My-MBP.local>
2021-05-22 17:15:17 +0200

    Separated parse graph logic from making pandas dataframe

--- src/commands/cmd_exporter.py
+++ src/commands/cmd_exporter.py
@@ -11,7 +11,7 @@ class Context:
         self.svc_triplestore = service_triplestore()
 
 
-def export_rdf_graphs_from_fuseki_server(ctx):
+def export_graphs_from_fuseki_server(ctx):
     response = ctx.obj.svc_triplestore.get_hazop_graph_bindings()
 
     if not bool(response):
@@ -30,13 +30,14 @@ def export_rdf_graphs_from_fuseki_server(ctx):
         graph = ctx.obj.svc_triplestore.get_hazop_graph(filename)
 
         index = config["HAZOP"]["new_index"]
-        filename = filename.replace(".ttl", ".xlsx")
-        df_graph = ctx.obj.svc_exporter.create_hazop_dataframe(graph, index)
-        ctx.obj.svc_exporter.export_to_excel(df_graph, filename)
-        click.echo("Saved file in data/excel directory: {}".format(filename))
+        graph_parsed = ctx.obj.svc_exporter.parse_graph(graph)
+        df = ctx.obj.svc_exporter.create_hazop_dataframe(graph_parsed, index)
+        df.name = filename.replace(".ttl", ".xlsx")
+        ctx.obj.svc_exporter.export_to_excel(df)
+        click.echo("Saved file in data/excel directory: {}".format(df.name))
 
 
-def export_rdf_graphs_from_local_directory(ctx):
+def export_graphs_from_local_directory(ctx):
     list_of_graphs = ctx.obj.svc_exporter.read_turtle_data()
 
     if not bool(list_of_graphs):
@@ -47,11 +48,11 @@ def export_rdf_graphs_from_local_directory(ctx):
             graph = f.read()
 
         index = config["HAZOP"]["new_index"]
-        filename = filepath.replace("data/turtle/",
-                                    "").replace(".ttl", ".xlsx")
-        df_graph = ctx.obj.svc_exporter.create_hazop_dataframe(graph, index)
-        ctx.obj.svc_exporter.export_to_excel(df_graph, filename)
-        click.echo("Saved file in data/excel directory: {}".format(filename))
+        graph_parsed = ctx.obj.svc_exporter.parse_graph(graph)
+        df = ctx.obj.svc_exporter.create_hazop_dataframe(graph_parsed, index)
+        df.name = filepath.replace("data/turtle/", "").replace(".ttl", ".xlsx")
+        ctx.obj.svc_exporter.export_to_excel(df)
+        click.echo("Saved file in data/excel directory: {}".format(df.name))
 
 
 @click.group()
@@ -63,13 +64,13 @@ def cli(ctx):
 
 @cli.command()
 @click.pass_context
-def cmd_export_rdf_graphs_from_fuseki_server(ctx):
+def cmd_export_graphs_from_fuseki_server(ctx):
     """Export RDF-Graphs"""
-    export_rdf_graphs_from_fuseki_server(ctx)
+    export_graphs_from_fuseki_server(ctx)
 
 
 @cli.command()
 @click.pass_context
-def cmd_export_rdf_graphs_from_local_directory(ctx):
+def cmd_export_graphs_from_local_directory(ctx):
     """Export RDF-Graphs"""
-    export_rdf_graphs_from_local_directory(ctx)
+    export_graphs_from_local_directory(ctx)
--- src/services/svc_exporter.py
+++ src/services/svc_exporter.py
@@ -6,19 +6,23 @@ class Service:
     def read_turtle_data(self):
         return glob.glob("data/turtle/*.ttl")
 
-    def create_hazop_dataframe(self, graph, index):
-        df = pd.DataFrame(columns=index)
-
+    def parse_graph(self, graph):
         g = Graph()
         g.parse(data=graph, format="turtle")
 
         with open("src/queries/hazop.rq", "r") as f:
             qres = g.query(f.read())
 
-        for i, row in enumerate(qres):
-            df.loc[i] = row
+        rows = []
+        for row in qres:
+            rows.append(row)
+
+        return rows
+
+    def create_hazop_dataframe(self, data, index):
+        df = pd.DataFrame(data, columns=index)
 
         return df
 
-    def export_to_excel(self, df, filename):
-        df.to_excel(f"data/excel/{filename}", index=False)
+    def export_to_excel(self, df):
+        df.to_excel(f"data/excel/{df.name}", index=False)
