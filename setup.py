7d06123fe6193275f902cb14f47ef70d381f1cdf
DimakDev <private.dk@icloud.com>
2021-05-24 15:52:35 +0200

    Update project credentials

--- setup.py
+++ setup.py
@@ -9,8 +9,34 @@ def read_requirements():
     return requirements
 
 
+def get_author():
+    author = ["Dmytro Kostiuk",
+              "Vincenz Christian Forkel",
+              "Artan Kabashi",
+              "Marcus Rothhaupt"]
+
+    author_str = ", ".join(author)
+
+    return author_str
+
+
+def get_author_email():
+    author_email = ["dmytro.kostiuk@mailbox.tu-dresden.de",
+                    "vincenz_christian.forkel@mailbox.tu-dresden.de",
+                    "marcus.rothhaupt@mailbox.tu-dresden.de",
+                    "artan.kabashi@mailbox.tu-dresden.de"]
+
+    author_email_str = ", ".join(author_email)
+
+    return author_email_str
+
+
 setup(name="cli",
-      version="0.1.0",
+      version="0.2.0",
+      author=get_author(),
+      author_email=get_author_email(),
+      description="CAE-PA",
+      platforms=["Windows", "macOS"],
       packages=find_packages(),
       include_package_date=True,
       install_requires=read_requirements(),
