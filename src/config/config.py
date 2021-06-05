417ae4128340e1765da47c2a52ccedb5a09d96a5
DimakDev <private.dk@icloud.com>
2021-06-05 19:14:18 +0200

    Updated config description. Described config parameters, valiation header and output header

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,21 @@
-"""Config file for Excel data parsing and validation"""
+"""The file containing parameters for reading and validating Excel data.
+
+Excel configuration (excel)
+
+    Parameters:
+     excel[*]["engine"],
+     excel[*]["header"],
+     excel[*]["sheet_name"],
+    are used in the function svc_importer.Service.read_hazop_dataframe to find and read the HAZOP dataframe in Excel workbook
+
+     excel[*]["valid_header"],
+    is a validation schema for the function svc_importer.Service.build_hazop_graph, the schema has to match the RDF schema in that function.
+
+Output header (output_header) contains all the headers used for the output Excel data.
+
+To update the program with another input data format (for exmaple .xsl). Make a new section ".xsl" with parameters in the excel configuration (excel) below and update src_importer.Service.read_excel_data to read and return a list of .xsl files.
+"""
+
 excel = {
     ".xlsb": {
         "engine": "pyxlsb",
