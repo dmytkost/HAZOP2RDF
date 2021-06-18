6eeee25b414805bb0a9a26ec0c1839aca60e8289
DimakDev <private.dk@icloud.com>
2021-06-18 02:48:24 +0200

    Docs: restructured directory

--- docs/bash.sh
+++ docs/bash.sh
@@ -1,10 +1,10 @@
 # required Pandoc, TeX Live
 
-pandoc	--bibliography=bibliography/bibliography.bib \
-		--metadata-file=metadata/metadata.yaml \
-		markdown/*.md -o HAZOP2RDF_report.pdf \
+pandoc	--bibliography=bibliography.bib \
+		--metadata-file=metadata.yaml \
+		*.md -o HAZOP2RDF_report.pdf \
 		--from=markdown \
-		--template=template/eisvogel.tex \
+		--template=eisvogel.tex \
 		--citeproc \
 		--listings \
 		--top-level-division=chapter \
